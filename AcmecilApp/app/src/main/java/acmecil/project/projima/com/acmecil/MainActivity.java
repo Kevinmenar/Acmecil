c764841a94112b9d664ee82d00a1442af3aaac08
Jouse <josuecanalesmena@gmail.com>
2019-03-27 09:39:18 -0600

    Aplicacion + Login Layout

---
+++ AcmecilApp/app/src/main/java/acmecil/project/projima/com/acmecil/MainActivity.java
@@ -0,0 +1,15 @@
+package acmecil.project.projima.com.acmecil;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import acmecil.project.projima.com.acmecil.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ AcmecilApp/app/src/main/java/acmecil/project/projima/com/acmecil/login/LogInActivity.java
@@ -0,0 +1,15 @@
+package acmecil.project.projima.com.acmecil.login;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import acmecil.project.projima.com.acmecil.R;
+
+public class LogInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in);
+    }
+}
