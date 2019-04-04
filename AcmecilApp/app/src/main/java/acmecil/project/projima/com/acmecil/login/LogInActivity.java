0a61c70bbe437352ce6fe32f3b61a91a7fd49ec3
jusola2 <juanjosesolano@hotmail.com>
2019-04-04 16:21:41 -0600

    log in finish
    ocupopo que alguien lo pruebe

--- AcmecilApp/app/src/main/java/acmecil/project/projima/com/acmecil/login/LogInActivity.java
+++ AcmecilApp/app/src/main/java/acmecil/project/projima/com/acmecil/login/LogInActivity.java
@@ -7,9 +7,47 @@ import acmecil.project.projima.com.acmecil.R;
 
 public class LogInActivity extends AppCompatActivity {
 
+    Button b1,b3;
+    EditText ed1,ed2;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_log_in);
+        setContentView(R.layout.activity_main);
+
+        b1 = (Button)findViewById(R.id.button);
+        ed1 = (EditText)findViewById(R.id.editText);
+        ed2 = (EditText)findViewById(R.id.editText2);
+
+        b3 = (Button)findViewById(R.id.button3);
+        b3.setPaintFlags(b3.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean respuesta = log(ed1.getText().toString(),ed2.getText().toString() );
+                //if( ed1.getText().toString().equals("admin") && ed2.getText().toString().equals("admin")) {
+                if(respuesta){
+                    Toast.makeText(getApplicationContext(),"Redirecting...",Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(getApplicationContext(), "WrongCredentials",Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
+
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LogInScreen.this, Inscribirse.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+    private boolean log(String correo, String password){
+        //Aqui se llama a base de datos con el email y la contraseña para verificar si coinciden
+        return true;
     }
 }
--- AcmecilApp/app/src/main/java/acmecil/project/projima/com/acmecil/login/RegisterActivity.java
+++ AcmecilApp/app/src/main/java/acmecil/project/projima/com/acmecil/login/RegisterActivity.java
@@ -7,9 +7,48 @@ import acmecil.project.projima.com.acmecil.R;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    EditText ed1,ed2, ed3;
+    Button b1,b2;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+        setContentView(R.layout.activity_inscribirse);
+
+        final Intent intent = getIntent();
+
+        ed1 = (EditText)findViewById(R.id.editText);
+        ed2 = (EditText)findViewById(R.id.editText2);
+        ed3 = (EditText)findViewById(R.id.editText8);
+
+        b1 = (Button)findViewById(R.id.button2);
+
+        b2 = (Button)findViewById(R.id.button4);
+        b2.setPaintFlags(b2.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!ed1.getText().toString().isEmpty()&&!ed2.getText().toString().isEmpty()&&!ed3.getText().toString().isEmpty()){
+                    if(ed2.getText().toString().equals(ed3.getText().toString())){
+                        Toast.makeText(getApplicationContext(),"Creando usuario.",Toast.LENGTH_SHORT).show();//aqui se pone el create user
+                    }else{
+                        Toast.makeText(getApplicationContext(),"La contraseñas no concuerdan.",Toast.LENGTH_SHORT).show();
+                    }
+
+                }else{
+                    Toast.makeText(getApplicationContext(),"Debe ingresar datos.",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
