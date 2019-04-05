b0d4635879d3ff0cbf3139bc7a2b0091b9181a61
marypaz98 <marypaz-aghe@hotmail.com>
2019-04-04 23:47:26 -0600

    Eliminar cuenta
    Cambios en eliminar usuario

--- AcmecilApp/app/src/main/java/acmecil/project/projima/com/acmecil/EliminarUsuario.java
+++ AcmecilApp/app/src/main/java/acmecil/project/projima/com/acmecil/EliminarUsuario.java
@@ -22,7 +22,7 @@ public class EliminarUsuario extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_eliminate_user);
 
         b1 = (Button)findViewById(R.id.button5);
         b2 = (Button)findViewById(R.id.button6);
