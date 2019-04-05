2068f83d52b95f7413408923bc8a329b8b27177a
marypaz98 <marypaz-aghe@hotmail.com>
2019-04-05 09:33:50 -0600

    Seleccionar farmacia

--- AcmecilApp/app/src/main/java/acmecil/project/projima/com/acmecil/MainActivity.java
+++ AcmecilApp/app/src/main/java/acmecil/project/projima/com/acmecil/MainActivity.java
@@ -5,6 +5,9 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import java.util.ResourceBundle;
 
@@ -16,19 +19,29 @@ import acmecil.project.projima.com.acmecil.login.LogInActivity;
 public class MainActivity extends AbsRuntimePermission {
 
     private final int PERMISSIONS_REQUEST_MAP = 6546;
+    ListView listView;
+    String [] choose = new String[]{"Buscar medicamento","Inscribir medicamento","Registrar publicidad","Eliminar usuario"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        listView = findViewById(R.id.listView);
         requestAppPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
                         Manifest.permission.ACCESS_COARSE_LOCATION,
                         Manifest.permission.READ_EXTERNAL_STORAGE},
                 R.string.msg,
                 PERMISSIONS_REQUEST_MAP);
-
         Controller.getInstance().setSessionRole(Controller.Role.COMMON_USER);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,choose);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+
+            }
+        });
 
     }
 
