e9377f10ce2d180f2085eb4c3e35a74e061ffef6
Jouse <josuecanalesmena@gmail.com>
2019-04-08 19:07:33 -0600

    Revert "Merge branch 'conect_to_data_base' of https://github.com/Kevinmenar/Acmecil into conect_to_data_base"
    This reverts commit 738a9335df7907be6b58fe37e7f6059adbcdd186, reversing
    changes made to b2dbb185e6c1796207058bc28314c68958acc677.

--- AcmecilApp/app/src/main/java/acmecil/project/projima/com/acmecil/SearchResultMapActivity.java
+++ AcmecilApp/app/src/main/java/acmecil/project/projima/com/acmecil/SearchResultMapActivity.java
@@ -3,7 +3,6 @@ package acmecil.project.projima.com.acmecil;
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Context;
-import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.BitmapFactory;
 import android.location.Location;
@@ -16,15 +15,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.widget.Toast;
 
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
-import com.google.firebase.database.ValueEventListener;
 import com.mapbox.android.core.permissions.PermissionsListener;
 import com.mapbox.android.core.permissions.PermissionsManager;
 import com.mapbox.geojson.Feature;
@@ -49,19 +41,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 import acmecil.project.projima.com.acmecil.Medicamentos.SearchResult;
-import acmecil.project.projima.com.acmecil.Medicamentos.buscarActivity;
 import acmecil.project.projima.com.acmecil.adapters.ResultListAdapter;
-import acmecil.project.projima.com.acmecil.model.Farmacia;
 
 public class SearchResultMapActivity extends AppCompatActivity implements PermissionsListener{
 
 
     private MapboxMap mapboxMap;
     private PermissionsManager permissionsManager;
-    private static final String TAG = "SearchResultMapActivity";
-    private List<SearchResult> testlist = new ArrayList<>();
-    private RecyclerView recyclerView;
-    private ResultListAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -75,13 +61,12 @@ public class SearchResultMapActivity extends AppCompatActivity implements Permis
         double latitude = location.getLatitude();
 
         //Recupera la lista de resultados
-        double logitud = getIntent().getDoubleExtra("log" , 0.0);
-        double lat = getIntent().getDoubleExtra("lat" , 0.0);
-        String marca = getIntent().getStringExtra("marca");
-        int radio = getIntent().getIntExtra("radio" , 0);
-        String medi = getIntent().getStringExtra("Medication");
+        List<SearchResult> testlist = (List<SearchResult>) getIntent().getBundleExtra("x").getSerializable("List");
 
-        searchMedication(marca, radio, medi, latitude, logitud);
+        ResultListAdapter adapter = new ResultListAdapter(testlist, this);
+        RecyclerView recyclerView = findViewById(R.id.search_results_recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+        recyclerView.setAdapter(adapter);
 
         // Create supportMapFragment
         SupportMapFragment mapFragment;
@@ -135,93 +120,6 @@ public class SearchResultMapActivity extends AppCompatActivity implements Permis
 
         
     }
-
-    public double distance(double lat1, double lat2, double lon1, double lon2) {
-
-        final int R = 6371; // Radius of the earth
-
-        double latDistance = Math.toRadians(lat2 - lat1);
-        double lonDistance = Math.toRadians(lon2 - lon1);
-        double a = Math.sin(latDistance / 2) * Math.sin(latDistance / 2)
-                + Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2))
-                * Math.sin(lonDistance / 2) * Math.sin(lonDistance / 2);
-        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-        double distance = R * c * 1000; // convert to meters
-
-        return distance;
-    }
-
-    //Función que llama al layout con los resultados del medicamento a buscar????
-    public void searchMedication(final String pMarca, final int pRadio, String pMedication, final double pLatitud, final double pLongitud){
-        DatabaseReference QueryAssociazioni = FirebaseDatabase.getInstance().getReference();
-        Query zonesQuery = QueryAssociazioni.child("Medicamentos").orderByChild("nombre").equalTo(pMedication);
-
-        zonesQuery.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                System.out.println("Info Gotit ");
-                Log.w(TAG, dataSnapshot.toString());
-                for (final DataSnapshot zoneSnapshot: dataSnapshot.getChildren()) {
-                    System.out.println("The value is: " + zoneSnapshot);
-                    Log.i(TAG, zoneSnapshot.child("nombre").getValue(String.class));
-
-                    // Get push id value.
-                    final String marca = zoneSnapshot.child("marca").getValue(String.class);
-                    final String precio = zoneSnapshot.child("price").getValue(String.class);
-                    final String nombre = zoneSnapshot.child("nombre").getValue(String.class);
-
-                    System.out.println("Marca " + marca);
-                    System.out.println("pMarca " + pMarca);
-
-                    if (marca.equals(pMarca)) {
-                        String keyIdFarmacia = zoneSnapshot.child("idFarmacia").getValue(String.class);
-                        System.out.println("keyIdFarmacia " + keyIdFarmacia);
-                        DatabaseReference QueryFarmacia = FirebaseDatabase.getInstance().getReference();
-                        Query Famacias = QueryFarmacia.child("Pharmacy").child(keyIdFarmacia);
-
-                        Famacias.addValueEventListener(new ValueEventListener() {
-
-                            @Override
-                            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                                System.out.println("dataSnapshot " + dataSnapshot );
-                                if(dataSnapshot!=null){
-                                    String nombre = dataSnapshot.child("nombre").getValue(String.class);
-                                    String direccion = dataSnapshot.child("direccion").getValue(String.class);
-                                    double latitud = dataSnapshot.child("latitud").getValue(double.class);
-                                    double longitud = dataSnapshot.child("longitud").getValue(double.class);
-                                    Farmacia farmacia = new Farmacia(nombre, latitud, longitud, direccion);
-                                    SearchResult searchResult = new SearchResult("", "", "", nombre , Integer.valueOf(precio), farmacia);
-                                    testlist.add(searchResult);
-                                    double distance = distance(pLatitud, latitud, pLongitud, longitud);
-                                    System.out.println("distance " + distance);
-
-                                    if(pRadio*1000>=distance) {
-                                        adapter = new ResultListAdapter(testlist, getApplicationContext());
-                                        recyclerView = findViewById(R.id.search_results_recycler_view);
-                                        recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
-                                        recyclerView.setAdapter(adapter);
-                                    }
-                                }
-                            }
-
-                            @Override
-                            public void onCancelled(@NonNull DatabaseError databaseError) {
-
-                            }
-                        });
-                    }
-                }
-
-                //  Logic here
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-                Log.w(TAG, "onCancelled", databaseError.toException());
-            }
-        });
-    }
-
 
     @SuppressWarnings( {"MissingPermission"})
     private void enableLocationComponent(@NonNull Style loadedMapStyle) {
